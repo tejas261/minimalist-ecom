92dcb17d64bfac63e371a1bf6dc8fbb0b371acd6
tejasm <tejasm085@gmail.com>
2026-03-15 18:30:41 +0530

    use existing acr details

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,13 +18,9 @@ resource "azurerm_resource_group" "rg" {
   location = "southindia"
 }
 
-# Azure Container Registry
-resource "azurerm_container_registry" "acr" {
+data "azurerm_container_registry" "acr" {
   name                = "minimalist"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-  admin_enabled       = true
+  resource_group_name = "acr"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -46,7 +42,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Connect AKS to ACR (allow AKS to pull images from ACR)
 resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.acr.id
+  scope = data.azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
