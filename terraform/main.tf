8bc655194c7a51e01b2d0cbcc76d3c7f1b5b806c
tejasm <tejasm085@gmail.com>
2026-03-15 16:31:00 +0530

    added terraform files to provision resources

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">=1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-minimal-rg"
+  location = "centralindia"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "minimal-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "minimalaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Connect AKS to ACR
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+data "azurerm_container_registry" "acr" {
+  name                = "minimalist"
+  resource_group_name = "acr"
+}
